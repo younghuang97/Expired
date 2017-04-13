844fcf32a0b82c847a6d98d55855a919f537a15f
Young <younghuang97@gmail.com>
2017-04-13 09:56:38 -0700

    Can now pull information from database in app

--- Expired!/front-end/app/src/main/java/com/example/thele/myfirstapp/AddActivity.java
+++ Expired!/front-end/app/src/main/java/com/example/thele/myfirstapp/AddActivity.java
@@ -2,12 +2,55 @@ package com.example.thele.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class AddActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    String server_url = "http://128.54.230.9/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        button = (Button)findViewById(R.id.btn);
+        textView = (TextView)findViewById(R.id.txt);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final RequestQueue requestQueue = Volley.newRequestQueue(AddActivity.this);
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+
+                        new Response.Listener<String>() {
+
+                            @Override
+                            public void onResponse(String response) {
+
+                                textView.setText(response);
+                                requestQueue.stop();
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        textView.setText("Something went wrong...");
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                });
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 }
--- Expired!/front-end/app/src/main/java/com/example/thele/myfirstapp/MainActivity.java
+++ Expired!/front-end/app/src/main/java/com/example/thele/myfirstapp/MainActivity.java
@@ -1,64 +1,25 @@
 package com.example.thele.myfirstapp;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-
 public class MainActivity extends AppCompatActivity {
 
-    final String TAG = this.getClass().getSimpleName();
+    //final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        /// databasing
-        String url = "https://10.0.2.2/project/index.php";
-
-        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d(TAG, response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "Error while reading google",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        MySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
 
     @Override
