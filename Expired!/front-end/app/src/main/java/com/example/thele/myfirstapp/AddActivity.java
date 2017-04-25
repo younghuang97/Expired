c2c8ea42b96da8ff414e79d923102d8e1a7842ba
Young <younghuang97@gmail.com>
2017-04-24 17:09:10 -0700

    Implemented user input to query database

--- Expired!/front-end/app/src/main/java/com/example/thele/myfirstapp/AddActivity.java
+++ Expired!/front-end/app/src/main/java/com/example/thele/myfirstapp/AddActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -13,43 +15,84 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddActivity extends AppCompatActivity {
     Button button;
-    TextView textView;
-    String server_url = "http://128.54.230.9/login.php";
+    EditText editText;
+    TextView fridgeTxt;
+    TextView frozenTxt;
+    String item_name;
+    String server_url = "http://128.54.238.234/query.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         button = (Button)findViewById(R.id.btn);
-        textView = (TextView)findViewById(R.id.txt);
+        editText = (EditText)findViewById(R.id.findTxt);
+        fridgeTxt = (TextView)findViewById(R.id.friTxt);
+        frozenTxt = (TextView)findViewById(R.id.froTxt);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                final RequestQueue requestQueue = Volley.newRequestQueue(AddActivity.this);
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                item_name = editText.getText().toString();
+                if (item_name.equals(""))
+                {
+                    fridgeTxt.setText("No input.");
+                    frozenTxt.setText("");
+                }
+                else {
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
 
-                        new Response.Listener<String>() {
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    try {
+                                        JSONArray jsonArray = new JSONArray(response);
+                                        JSONObject jsonObject = jsonArray.getJSONObject(0);
+                                        String fridge = jsonObject.getString("fridge");
+                                        if (fridge.equals("null"))
+                                        {
+                                            fridgeTxt.setText("Item isn't stored in database.");
+                                            frozenTxt.setText("");
+                                        }
+                                        else {
+                                            String frozen = jsonObject.getString("frozen");
+                                            fridge = fridge + " days refrigerated.";
+                                            frozen = frozen + " days frozen.";
+                                            fridgeTxt.setText(fridge);
+                                            frozenTxt.setText(frozen);
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
 
-                            @Override
-                            public void onResponse(String response) {
+                            fridgeTxt.setText("Something went wrong...");
+                            error.printStackTrace();
+                        }
+                    }){
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            Map<String, String> params = new HashMap<>();
+                            params.put("name", item_name);
 
-                                textView.setText(response);
-                                requestQueue.stop();
-
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                        textView.setText("Something went wrong...");
-                        error.printStackTrace();
-                        requestQueue.stop();
-                    }
-                });
-                requestQueue.add(stringRequest);
+                            return params;
+                        }
+                    };
+                    MySingleton.getInstance(AddActivity.this).addToRequestQueue(stringRequest);
+                }
             }
         });
     }
---
+++ Expired!/www/conn.php
@@ -0,0 +1,15 @@
+<?php
+    $db_name = "item";
+    $mysql_username = "root";
+    $mysql_password = "";
+    $server_name = "localhost";
+    $mysqli = new mysqli($server_name, $mysql_username, $mysql_password, 
+        $db_name);
+    if ($mysqli->connect_errno) {
+        echo "Connect failed: $mysqli->connect->error\n";
+        exit();
+    }
+    else {
+        #echo "Connect success!\n";
+    }
+?>
