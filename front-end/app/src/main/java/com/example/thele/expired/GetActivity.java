654a752a9ce7d3356b951624e77e5a87dbc7c43b
Young <younghuang97@gmail.com>
2017-08-01 23:57:29 -0700

    Reformatted gradle.

--- front-end/app/src/main/java/com/example/thele/expired/GetActivity.java
+++ front-end/app/src/main/java/com/example/thele/expired/GetActivity.java
@@ -7,19 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class GetActivity extends AppCompatActivity {
     Button button;
     EditText editText;
