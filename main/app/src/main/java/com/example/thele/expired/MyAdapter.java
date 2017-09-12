922a9a1eb51c24daaab9e486ce902a4c69bd3c82
Young <younghuang97@gmail.com>
2017-09-11 23:10:01 -0700

    Editing an item now displays toasts when a field is empty.

--- main/app/src/main/java/com/example/thele/expired/MyAdapter.java
+++ main/app/src/main/java/com/example/thele/expired/MyAdapter.java
@@ -3,6 +3,7 @@ package com.example.thele.expired;
 import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>
 {
+    private String TAG = "MyAdapter";
     private List<Item> mDataset;
     private Context mContext;
 
@@ -63,26 +65,104 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>
                 public void onClick(View v) {
                     AlertDialog.Builder mBuilder = new AlertDialog.Builder(mContext);
                     View mView = LayoutInflater.from(mContext).inflate(R.layout.dialog_edititem, null);
-                    mBuilder.setView(mView);
-                    mBuilder.show();
-                    EditText itemName = (EditText) mView.findViewById(R.id.etName);
-                    EditText purDate = (EditText) mView.findViewById(R.id.etPurchaseDate);
-                    EditText expDate = (EditText) mView.findViewById(R.id.etExpirationDate);
-                    EditText storageType = (EditText) mView.findViewById(R.id.etStorageType);
+                    final EditText itemName = (EditText) mView.findViewById(R.id.etName);
+                    final EditText purDate = (EditText) mView.findViewById(R.id.etPurchaseDate);
+                    final EditText expDate = (EditText) mView.findViewById(R.id.etExpirationDate);
+                    final EditText storageType = (EditText) mView.findViewById(R.id.etStorageType);
                     Button editBtn = (Button) mView.findViewById(R.id.editbutton);
-                    /*
-                    itemName.setText();
-                    purDate.setText();
-                    expDate.setText();
-                    storageType.setText();*/
+
+                    itemName.setText(holder.nameView.getText().toString().trim());
+                    purDate.setText(holder.purDateView.getText().toString().trim());
+                    expDate.setText(holder.expDateView.getText().toString().trim());
+                    storageType.setText(item.getStorageType());
 
                     editBtn.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                            //changes to item occur here
+                            String name = itemName.getText().toString().trim();
+                            String pDate = purDate.getText().toString().trim();
+                            String eDate = expDate.getText().toString().trim();
+                            String sType = storageType.getText().toString().trim();
+
+                            // checks for empty fields
+                            String emptyStringMessage = "The following fields are empty: ";
+                            Toast emptyToast = Toast.makeText(mContext, emptyStringMessage, Toast.LENGTH_LONG);
+                            boolean empty = false;
+                            boolean firstEmpty = true; // used to track when to add a comma
+                            if (name.isEmpty())
+                            {
+                                firstEmpty = false;
+                                emptyStringMessage += "Name";
+                                emptyToast.setText(emptyStringMessage);
+                                empty = true;
+                            }
+                            if (pDate.isEmpty())
+                            {
+                                if (!firstEmpty)
+                                {
+                                    emptyStringMessage += ", Purchase Date";
+                                }
+                                else
+                                {
+                                    emptyStringMessage += "Purchase Date";
+                                    firstEmpty = false;
+                                }
+                                emptyToast.setText(emptyStringMessage);
+                                empty = true;
+                            }
+                            if (eDate.isEmpty())
+                            {
+                                if (!firstEmpty)
+                                {
+                                    emptyStringMessage += ", Expiration Date";
+                                }
+                                else
+                                {
+                                    emptyStringMessage += "Expiration Date";
+                                    firstEmpty = false;
+                                }
+                                emptyToast.setText(emptyStringMessage);
+                                empty = true;
+                            }
+                            if (sType.isEmpty())
+                            {
+                                if (!firstEmpty)
+                                {
+                                    emptyStringMessage += ", Storage Type";
+                                }
+                                else
+                                {
+                                    emptyStringMessage += "Storage Type";
+                                }
+                                emptyToast.setText(emptyStringMessage);
+                                empty = true;
+                            }
+                            // for proper grammar
+                            emptyStringMessage += '.';
+                            emptyToast.setText(emptyStringMessage);
+
+                            if (empty)
+                            {
+                                emptyToast.show();
+                            }
+
+
+                            // check for valid date(exp date should be at least after today's date)
+                            else
+                            {
+
+                            }
+                            // check for valid storage types.)
+
+                            // replaces the item's details where applicable
+
+                            // update main activity to reflect changes
                         }
                     });
 
+                    mBuilder.setView(mView);
+                    AlertDialog dialog = mBuilder.create();
+                    dialog.show();
                 }
             });
             holder.tinyDeleteButton.setOnClickListener(new View.OnClickListener() {
